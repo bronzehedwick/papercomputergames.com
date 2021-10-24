f3152b8a430f01c5cab51ea97bd5b638829dc5c0
Chris DeLuca <code@chrisdeluca.me>
2021-10-24 12:35:55 -0400

    Teach the svg build to deal with multiple targets
    When compiling this poor-man's mustache replacement syntax, have the
    script loop over multiple html files to do the replacement as well.
    
    Improve the compiling by removing new lines, tabs, and returns from
    source svgs, which break the basic `sed` replacements.
    
    Do this transformation in a temporary directory that gets cleaned up at
    the end of each run.
    
    Add the unofficial bash strict mode to the script.

--- svg-templates.sh
+++ svg-templates.sh
@@ -1,10 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1
+
+svgtmpdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'pcgsvg')
 
 # Replace {{ mustache }} syntax in index.html with corresponding SVGs
-find ./src/_svgs -type f -name "*.svg" | while read -r file; do
-  name="$(basename "$file" | cut -d . -f 1)"
-  contents="$(cat "$file")"
-  echo "$name"
-  sed -i.bak "s;{{ $name }};$contents;g" ./public/index.html
-  if [ -f ./public/index.html.bak ]; then rm public/index.html.bak; fi
+find ./src/_svgs -type f -name "*.svg" | while read -r svg; do
+  svgname="$(basename "$svg" | cut -d . -f 1)"
+  tr -d "\n\t\r" < "$svg" > "$svgtmpdir/$svgname.svg"
+  contents="$(cat "$svgtmpdir/$svgname.svg")"
+  echo "$contents"
+  find ./src/ -type f -name "*.html" | while read -r file; do
+    htmlname="$(basename "$file" | cut -d . -f 1)"
+    sed -i.bak "s;{{ $svgname }};$contents;g" "./public/$htmlname.html"
+    if [ -f "./public/$htmlname.html.bak" ]; then rm "public/$htmlname.html.bak"; fi
+  done
 done
+
+rm -rf "$svgtmpdir"
